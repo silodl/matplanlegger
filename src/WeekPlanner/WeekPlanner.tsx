24f53e7d02bf1d561f5b5a419b44ee622d728c94
silodl <silje.odland@hotmail.no>
2022-09-11 15:06:32 +0200

    small design fixes

--- src/WeekPlanner/WeekPlanner.tsx
+++ src/WeekPlanner/WeekPlanner.tsx
@@ -5,11 +5,12 @@ import './WeekPlanner.css';
 import { GetWeek } from './GetWeek';
 
 const GetWeekNumber = () => {
-  let currentDate = new Date();
-  let dayInWeek1 = new Date(currentDate.getFullYear(), 0, 4); // 4. jan is always in week 1
-  let firstDay = new Date(dayInWeek1.getTime() - dayInWeek1.getDay() * 24 * 60 * 60 * 1000); // get first date in week 1
-  let days = Math.ceil((currentDate.getTime() - firstDay.getTime()) / (24 * 60 * 60 * 1000));
-  let weekNumber = Math.ceil(days / 7);
+  const now = new Date();
+  const today =  new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0)
+  const dayInWeek1 = new Date(today.getFullYear(), 0, 4); // 4. jan is always in week 1
+  const firstDay = new Date(dayInWeek1.getTime() - dayInWeek1.getDay() * 24 * 60 * 60 * 1000); // get first date in week 1
+  const days = Math.ceil((today.getTime() - firstDay.getTime()) / (24 * 60 * 60 * 1000));
+  const weekNumber = Math.ceil(days / 7);
   return weekNumber;
 }
 
